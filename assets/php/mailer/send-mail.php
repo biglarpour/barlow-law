704a6742a5835342e4ded2b956fb061452edf847
biglarpour <biglarpour@gmail.com>
2018-04-06 20:01:17 -1000

    more tests for phpmailer

--- assets/php/mailer/send-mail.php
+++ assets/php/mailer/send-mail.php
@@ -1,21 +1,16 @@
 <?php
 require_once('./class.phpmailer.php');
-
+echo ($_SERVER['REQUEST_METHOD'] == 'POST');
 try {
-    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $name = trim($_POST['name']);
-        $from_email = trim($_POST['email']);
-        $message = trim($_POST['message']);
-        $gotcha = trim($_POST['gotcha']);
-        echo $_SERVER['REQUEST_METHOD'];
-        echo $_POST['email'];
-        echo $_POST['name'];
-        echo $_POST['gotcha'];
-        echo $_POST['message'];
-        }
-    else{
-        echo "Must be a post"
-        }
+    $name = trim($_POST['name']);
+    $from_email = trim($_POST['email']);
+    $message = trim($_POST['message']);
+    $gotcha = trim($_POST['gotcha']);
+    echo $_SERVER['REQUEST_METHOD'];
+    echo $_POST['email'];
+    echo $_POST['name'];
+    echo $_POST['gotcha'];
+    echo $_POST['message'];
     }
 catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
