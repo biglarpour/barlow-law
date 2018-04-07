3710a657deeaa6d2a2e72df1b3c293110ddb9cab
biglarpour <biglarpour@gmail.com>
2018-04-06 19:42:03 -1000

    Adds first full version site of barlowlaw.co

---
+++ assets/php/mailer/send-mail.php
@@ -0,0 +1,39 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    require_once('./class.phpmailer.php');
+
+    $name = trim($_POST['name']);
+    $from_email = trim($_POST['email']);
+    $message = trim($_POST['message']);
+    $gotcha = trim($_POST['gotcha']);
+    if (){
+    //PHPMailer Object
+    $mail = new PHPMailer;
+    //From email address and name
+    $mail->From = $from_email;
+    $mail->FromName = $name;
+
+    //To address and name
+    $mail->addAddress("garrett@barlowlaw.co", "Garrett Barlow");
+
+    //Address to which recipient will reply
+    $mail->addReplyTo($from_email, "Reply");
+
+    //Send HTML or Plain Text email
+    $mail->isHTML(true);
+
+    $mail->Subject = "BarlowLaw.co " . $name . "wants to get in touch";
+    $mail->Body = "<i>" . $message . "</i>";
+    $mail->AltBody = $message;
+    if (!empty($gotcha){
+        if(!$mail->send()) {
+            echo "Mailer Error: " . $mail->ErrorInfo;
+        }
+        else {
+            echo "Message has been sent successfully";
+        }
+    }
+    else {
+        echo "You do not have permission";
+    }
+?>
